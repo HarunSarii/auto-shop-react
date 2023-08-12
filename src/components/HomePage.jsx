1cc08373b77c56604b61b0e15a5b9f59d57fcc29
HarunSarii <harunsari14@gmail.com>
2023-08-13 02:02:52 +0300

    feat: add pagination to homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
-
 import axios from "axios";
 import Product from "./Product";
 
 const HomePage = () => {
   const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
 
   useEffect(() => {
     axios
@@ -17,13 +18,71 @@ const HomePage = () => {
       });
   }, []);
 
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
+
+  const totalPages = Math.ceil(products.length / productsPerPage);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
   return (
     <div className="container">
       <div className="row">
-        {products.map((product) => (
+        {currentProducts.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </div>
+      <nav className="d-flex justify-content-center mt-4">
+        <ul className="pagination">
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              &laquo;
+            </button>
+          </li>
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <li
+              key={index}
+              className={`page-item ${
+                currentPage === index + 1 ? "active" : ""
+              }`}
+            >
+              <button
+                className={`page-link ${
+                  currentPage === index + 1 ? "text-blue-500" : "text-black"
+                }`}
+                onClick={() => handlePageChange(index + 1)}
+              >
+                {index + 1}
+              </button>
+            </li>
+          ))}
+          <li
+            className={`page-item ${
+              currentPage === totalPages ? "disabled" : ""
+            }`}
+          >
+            <button
+              className="page-link"
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPages}
+            >
+              &raquo;
+            </button>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 };
