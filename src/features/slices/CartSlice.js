4fa223cda3d4e5a4b11ad01d17351aa6d25e4c95
HarunSarii <harunsari14@gmail.com>
2023-08-13 22:26:13 +0300

    feat: add cart slice

---
+++ src/features/slices/CartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const newItem = action.payload;
+      state.cartItems.push(newItem);
+    },
+    removeFromCart: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+  },
+});
+
+export const { addToCart, removeFromCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchReducer from "./slices/SearchSlice";
 import filterReducer from "./slices/FilterSlice";
+import cartReducer from "../features/slices/CartSlice"; // Import the cart slice
 
 export const store = configureStore({
   reducer: {
     search: searchReducer,
     filter: filterReducer,
+    cart: cartReducer,
   },
 });
